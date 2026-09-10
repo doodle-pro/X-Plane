0426af835bfea6f0c4e3408138141c76084b7ef7
doodle-pro <msubhranil065@gmail.com>
2026-09-10 12:50:00 +0530

    feat(gameplay): implement Particle effects

--- src/script.js
+++ src/script.js
@@ -749,3 +749,103 @@ EnnemiesHolder.prototype.rotateEnnemies = function () {
             this.mesh.remove(ennemy.mesh);
             i--;
         }
+    }
+}
+
+class Particle {
+    constructor () {
+        var geom = new THREE.TetrahedronGeometry(3, 0);
+        var mat = new THREE.MeshPhongMaterial({
+            color: 0x009999,
+            shininess: 0,
+            specular: 0xffffff,
+            shading: THREE.FlatShading
+        });
+        this.mesh = new THREE.Mesh(geom, mat);
+    }
+}
+
+Particle.prototype.explode = function (pos, color, scale) {
+    var _this = this;
+    var _p = this.mesh.parent;
+    this.mesh.material.color = new THREE.Color(color);
+    this.mesh.material.needsUpdate = true;
+    this.mesh.scale.set(scale, scale, scale);
+    var targetX = pos.x + (-1 + Math.random() * 2) * 50;
+    var targetY = pos.y + (-1 + Math.random() * 2) * 50;
+    var speed = .6 + Math.random() * .2;
+    TweenMax.to(this.mesh.rotation, speed, { x: Math.random() * 12, y: Math.random() * 12 });
+    TweenMax.to(this.mesh.scale, speed, { x: .1, y: .1, z: .1 });
+    TweenMax.to(this.mesh.position, speed, {
+        x: targetX, y: targetY, delay: Math.random() * .1, ease: Power2.easeOut, onComplete: function () {
+            if (_p) _p.remove(_this.mesh);
+            _this.mesh.scale.set(1, 1, 1);
+            particlesPool.unshift(_this);
+        }
+    });
+}
+
+class ParticlesHolder {
+    constructor () {
+        this.mesh = new THREE.Object3D();
+        this.particlesInUse = [];
+    }
+}
+
+ParticlesHolder.prototype.spawnParticles = function (pos, density, color, scale) {
+
+    var nPArticles = density;
+    for (var i = 0; i < nPArticles; i++) {
+        var particle;
+        if (particlesPool.length) {
+            particle = particlesPool.pop();
+        } else {
+            particle = new Particle();
+        }
+        this.mesh.add(particle.mesh);
+        particle.mesh.visible = true;
+        var _this = this;
+        particle.mesh.position.y = pos.y;
+        particle.mesh.position.x = pos.x;
+        particle.explode(pos, color, scale);
+    }
+}
+
+class Coin {
+    constructor () {
+        var geom = new THREE.TetrahedronGeometry(5, 0);
+        var mat = new THREE.MeshPhongMaterial({
+            color: 0x009999,
+            shininess: 0,
+            specular: 0xffffff,
+
+            shading: THREE.FlatShading
+        });
+        this.mesh = new THREE.Mesh(geom, mat);
+        this.mesh.castShadow = true;
+        this.angle = 0;
+        this.dist = 0;
+    }
+}
+
+class CoinsHolder {
+    constructor (nCoins) {
+        this.mesh = new THREE.Object3D();
+        this.coinsInUse = [];
+        this.coinsPool = [];
+        for (var i = 0; i < nCoins; i++) {
+            var coin = new Coin();
+            this.coinsPool.push(coin);
+        }
+    }
+}
+
+CoinsHolder.prototype.spawnCoins = function () {
+
+    var nCoins = 1 + Math.floor(Math.random() * 10);
+    var d = game.seaRadius + game.planeDefaultHeight + (-1 + Math.random() * 2) * (game.planeAmpHeight - 20);
+    var amplitude = 10 + Math.round(Math.random() * 10);
+    for (var i = 0; i < nCoins; i++) {
+        var coin;
+        if (this.coinsPool.length) {
+            coin = this.coinsPool.pop();
